dd25e6f55dbd7a6d236111a68102e36bd928a868
martin madisson <Martin.Madisson@tpt.lan>
2026-03-05 13:56:20 +0200

    index.html

--- javascript.js
+++ javascript.js
@@ -1,9 +1,12 @@
 const container = document.querySelector('.container');
 
 container.addEventListener('click', function (e) {
-    console.log(e)
     if (e.target.tagName === 'BUTTON') {
         e.target.classList.toggle('selected');
     }
 });
 
+function sendBron() {
+return document.querySelector('button.selected').innerText                          // saada
+}
+console.log(sendBron)
