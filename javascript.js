0f8900e3c34730d6f5ddc2a07fae7288721b16cf
martin madisson <Martin.Madisson@tpt.lan>
2026-03-05 13:53:44 +0200

    javascript

--- javascript.js
+++ javascript.js
@@ -1,6 +1,7 @@
 const container = document.querySelector('.container');
 
 container.addEventListener('click', function (e) {
+    console.log(e)
     if (e.target.tagName === 'BUTTON') {
         e.target.classList.toggle('selected');
     }
