eedfe91ba27350f40fb74079437661d3e3cc9c4a
martin madisson <Martin.Madisson@tpt.lan>
2026-03-12 13:29:14 +0200

    index.html

--- javascript.js
+++ javascript.js
@@ -1,36 +1,36 @@
 const salvesta = document.querySelector("#salvesta")
 const nimi = document.querySelector("#nimi")
 const kohad = document.querySelector("#koht")
-let eesnimi = ""
-let istekoht = ""
+let eesnimi = "";
+let istekoht = "";
 
 salvesta.addEventListener('click', function() {
-    eesnimi = nimi.value
-    istekoht = kohad.value
-    save(eesnimi, istekoht)
-
+    eesnimi = nimi.value;
+    istekoht = kohad.value;
+    save(eesnimi, istekoht);
 });
 
 
-async function save (name, place) {
-const response = await fetch('https://tinkr.tech/sdb/istekohad', {
-  method: 'POST',
-  headers: { 'Content-Type': 'application/json' },
-  body: JSON.stringify({ name: name, places: place })
-});
+ HEAD
+async function save(name, place) {
+    const response = await fetch('https://tinkr.tech/sdb/istekohad', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ name: name, places: place })
+    });
 }
 
 async function load() {
-const response = await fetch('https://tinkr.tech/sdb/istekohad');
-const documents = await response.json();
-console.log(documents);
+    const response = await fetch('https://tinkr.tech/sdb/istekohad');
+    const documents = await response.json();
+    console.log(documents);
 
-for (const itms of documents){
-    const konteiner = document.querySelector("#container")
-    const iste = document.createElement("h3")
-    iste.textContent = "Nimi: " + itms.name + " Koht: " + itms.places
-    konteiner.appendChild(iste)
-}
+    for (const itms of documents) {
+        const konteiner = document.querySelector("#container");
+        const iste = document.createElement("h3");
+        iste.textContent = "Nimi: " + itms.name + " Koht: " + itms.places;
+        konteiner.appendChild(iste);
+    }
 }
 
 load()
